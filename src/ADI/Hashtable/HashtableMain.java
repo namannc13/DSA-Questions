3f881c92169af5af678ef21d7958d904d8c1fe34
chomchom13 <chawhannaman5@gmail.com>
2024-01-27 15:34:17 +0530

    removed HeapMain.java

--- src/ADI/Hashtable/HashtableMain.java
+++ src/ADI/Hashtable/HashtableMain.java
@@ -2,6 +2,7 @@ package ADI.Hashtable;
 
 import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.Map;
 
 public class HashtableMain {
     public static void main(String[] args) {
@@ -9,14 +10,28 @@ public class HashtableMain {
         
         hash.put("naman", 1);
         hash.put("nikhil", 2);
-        System.out.print(hash.containsKey("naman"));
+
+        System.out.print("contains naman or not " + hash.containsKey("naman"));
         System.out.println();
+
+        System.out.println("get value of key naman " + hash.get("naman"));
         
+        System.out.println(hash.remove("naman"));
         
         Enumeration<String> eNum = hash.keys(); // we are using Enumeration here to store the hashkeys seperately 
         
         while(eNum.hasMoreElements()){
-            System.out.print(eNum.nextElement() + " ");
+            String key = eNum.nextElement();
+            System.out.print("key : " + key + " value : " + hash.get(key));
         }
+
+        System.out.println();
+
+        System.out.println("Printing the hastable : " + hash);
+
+        System.out.println();
+        
+        for (Map.Entry<String, Integer> e : hash.entrySet()) System.out.println(e.getKey() + " " + e.getValue());
+        
     }
 }
