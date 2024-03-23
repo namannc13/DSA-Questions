b56d99088967fe21e30de4a19e81fe3135104b96
namannc13 <chawhannaman5@gmail.com>
2024-03-23 05:47:02 +0530

    subsetsWithDuplicates.java

--- src/Backtracking/subsetsWithDuplicates.java
+++ src/Backtracking/subsetsWithDuplicates.java
@@ -1,17 +1,22 @@
 package Backtracking;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class Subsets {
-    public static List<List<Integer>> subsets(int[] nums) {
+public class subsetsWithDuplicates {
+    public static List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
         subsets(nums, nums.length-1, ans, new ArrayList<>());
         return ans;
     }
     public static void subsets(int[] nums, int i, List<List<Integer>> ans, List<Integer> res){
-        if(i < 0){
-            ans.add(new ArrayList<>(res));
+        if (i < 0) {
+            List<Integer> sortedRes = new ArrayList<>(res); // Create a copy of res
+            Collections.sort(sortedRes); // Sort the copy
+            if (!ans.contains(sortedRes)) { // Check for presence of sortedRes in ans
+                ans.add(new ArrayList<>(sortedRes));
+            }
             return;
         }
         subsets(nums, i-1, ans,res);
@@ -22,8 +27,6 @@ public class Subsets {
     }
     public static void main(String[] args) {
         int[] arr = { 2, 4, 5};
-        System.out.println(subsets(arr));
+        System.out.println(subsetsWithDup(arr));
     }
 }
-
-
